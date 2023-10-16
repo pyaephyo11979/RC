9f74e6bfe74588201e9e67932b3534e5f776427a
pyaephyo11979 <www.pyaephyoehlaing16@gmail.com>
2023-10-16 23:53:40 +0630

    up

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ fetch("https://booklibraryapi.onrender.com/Book").then(res=>res.json())
         <img src="${book.image}" class="card-img img-fluid " style="width:200px;" alt="...">
           <h5  class="bookName card-title">${book.name}</h5>
           <p>${book.author.name}</p>
-          <a href="${book.pdf_url}" target="_blank" class="btn btn-primary">Read Now</a>`
+          <a href="${book.pdf_url}" target="_blank" class="btn" style="background-color:wheat;">Read Now <i class="fas fa-book-open"></i></a>`
     })
 })
+//Search
 sitem.addEventListener('keyup',(e)=>{
     let text=e.target.value;
     let sinput=text.toLowerCase();
@@ -43,6 +44,7 @@ sbtn.addEventListener('click',()=>{
         console.log(searchGiven.includes(sinput));
     })
 })
+//Choose Category
 let cattoggle=document.querySelectorAll('.cate');
 cattoggle.forEach((cat)=>{
     cat.addEventListener('click',()=>{
@@ -63,6 +65,7 @@ cattoggle.forEach((cat)=>{
         })
     })
 })
+//Choose Tags
 let tags=document.querySelectorAll('.tag');
 tags.forEach((tg)=>{
     tg.addEventListener('click',()=>{
